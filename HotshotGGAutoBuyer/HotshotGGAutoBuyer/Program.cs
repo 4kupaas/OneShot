d4cf96939dcc520b93521f7fc86f744072dfc226
YeezyYeezyWhatsGood <YeezyYeezy@RiotGamesSueMyCunt.com>
2016-11-25 03:52:22 +0100

    fssfasafasggsa

---
+++ HotshotGGAutoBuyer/HotshotGGAutoBuyer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotshotGGAutoBuyer
+{
+    using LeagueSharp;
+    using LeagueSharp.Common;
+    using ItemData = LeagueSharp.Common.Data.ItemData;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += OnLoad;
+        }
+
+        private static void OnLoad(EventArgs args)
+        {
+
+            Game.OnUpdate += OnUpdate;
+        }
+
+        private static void OnUpdate(EventArgs args)
+        {
+            if (ItemData.Corrupting_Potion.GetItem().IsOwned())
+            {
+                return;
+            }
+
+            ObjectManager.Player.BuyItem((ItemId)2033);
+        }
+    }
+}
